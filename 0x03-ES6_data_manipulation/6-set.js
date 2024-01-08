fbc03516b3b8ff00b114cceb08ecd2fbc645bb4e
Mary-Afrika <marygahar@gmail.com>
2024-01-08 08:28:56 -0800

    ALX

---
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -0,0 +1,9 @@
+/**
+ * Creates a set from an array.
+ * @param {Array} array - The source array.
+ * @author Mary Njoroge <https://github.com/Mary-Afrika>
+ * @returns {Set}
+ */
+export default function setFromArray(array) {
+  return new Set(array);
+}
